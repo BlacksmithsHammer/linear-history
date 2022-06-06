7371bfffb5f5907b4c8cb4ec680d3f9b67eeff6e
BlacksmithsHammer <denis.fatyushckin2019@yandex.ru>
2022-06-06 22:49:27 +0400

    now function fib include series from 0, 1, 1, 2, 3, 5... (early from 1, 1, 2, 3, 5...)

--- program.py
+++ program.py
@@ -1,7 +1,7 @@
-def fib(n):                                     #a function that returns the fibonacci number in a series with (1, 1, 2, 3, 5...)
-    if n > 2: return fib(n - 1) + fib(n - 2)
-    if n > 0: return 1
-
+def fib(n):                                     #a function that returns the fibonacci number in a series with (0, 1, 1, 2, 3, 5...)
+    if n > 3: return fib(n - 1) + fib(n - 2)
+    if n > 1: return 1
+    return 0
 
 print("Hello, World!")
 
