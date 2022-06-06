c6446bccbbc129e634f1d6140296c705c5df6df9
BlacksmithsHammer <denis.fatyushckin2019@yandex.ru>
2022-06-06 22:44:40 +0400

    added function fib that calculates fibonacci-number

--- program.py
+++ program.py
@@ -1,1 +1,8 @@
+def fib(n):                                     #a function that returns the fibonacci number in a series with (1, 1, 2, 3, 5...)
+    if n > 2: return fib(n - 1) + fib(n - 2)
+    if n > 0: return 1
+
+
 print("Hello, World!")
+
+print(fib(10))                                  #example of using fib(number)
